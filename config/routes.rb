d0a50f22897405c7d6bf6ac1c6b472fcb35a8db3
David <sanchez.dav90@gmail.com>
2019-07-07 21:21:17 -0500

    Create the unautheticated page and scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+
+  unauthenticated :user do
+    devise_scope :user do
+      root to: 'unauthenticated#index', as: :unauthenticated_root
+    end
+  end
+
+  authenticated :user do
+    root to: 'home#index', as: :authenticated_root
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

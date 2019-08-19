1e70054203b1feb62708e8009e244679a754ecc3
David <sanchez.dav90@gmail.com>
2019-08-18 23:15:41 -0500

    Add get_user_avatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
+
+  def get_user_avatar(size)
+    if current_user.avatar.attached?
+      image_tag(current_user.avatar, height: size, width: size, class: "rounded")
+    else
+      image_tag(gravatar_image_url(current_user.email, size: size * 2), height: size, width: size, class: "rounded")
+    end
+  end
 end

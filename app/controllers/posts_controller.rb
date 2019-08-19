a4b775cd631ee44539cb6570922ea0737b30a788
David <sanchez.dav90@gmail.com>
2019-08-18 23:15:08 -0500

    Add post edit view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   def index
@@ -24,7 +25,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.new(post_params)
 
     if @post.save
-      redirect_to authenticated_root_path, notice: 'Post was successfully created.'
+      redirect_to authenticated_root_path, notice: 'Question was successfully created.'
     else
       render partial: 'shared/error_messages', locals: {resource: @post}, status: :bad_request
     end
@@ -32,15 +33,26 @@ class PostsController < ApplicationController
 
   # PATCH/PUT /posts/:id
   def update
+    if @post.update(post_params)
+      redirect_to authenticated_root_path, notice: 'Question was successfully updated.'
+    else
+      render partial: 'shared/error_messages', locals: {resource: @post}, status: :bad_request
+    end
   end
 
   # DELETE /posts/:id
   def destroy
+    @post.destroy
+    redirect_to authenticated_root_path, notice: 'Question was successfully destroyed.'
   end
 
   private
 
     def post_params
       params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+      @post = current_user.posts.friendly.find(params[:id])
     end
 end
